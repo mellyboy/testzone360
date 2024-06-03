64579dac1d277e364149600edefa389d714c4898
mellyboy <mel.abanto19@gmail.com>
2024-06-03 23:41:44 +0800

    implemented textarea UI component

---
+++ src/views/beginner/textarea/Textarea.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+    CCard,
+    CCardBody,
+    CCardHeader,
+    CCol,
+    CFormTextarea,
+    CRow,
+} from '@coreui/react';
+import { IMaskMixin } from 'react-imask'
+
+const TextArea = () => {
+    return (
+        <CRow>
+            <CCol xs={12}>
+                <CCard className="mb-4">
+                    <CCardHeader>
+                        <strong>Text Area</strong>
+                    </CCardHeader>
+                    <CCardBody>
+                        <CFormTextarea
+                            placeholder="Leave a comment here"
+                            id="floatingTextarea2"
+                            style={{ height: '100px' }}
+                            text="Must be 15 - 1500 characters."
+                        ></CFormTextarea>
+                    </CCardBody>
+                </CCard>
+            </CCol>
+
+        </CRow>
+    );
+};
+
+export default TextArea
