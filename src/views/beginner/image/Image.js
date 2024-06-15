97b665e0a703fb63cf7ca9ffccb1a81c0c369e82
mellyboy <mel.abanto19@gmail.com>
2024-06-15 14:36:52 +0800

    update element ids

--- src/views/beginner/image/Image.js
+++ src/views/beginner/image/Image.js
@@ -11,26 +11,32 @@ const getRandomVisibility = () => Math.random() > 0.5; // 50% chance to show the
 const ImageGallery = () => {
     const images = [
         {
+            tag: "catimg1",
             src: catImg1,
             attribution: <small><a href="https://www.freepik.com/free-vector/hand-drawn-cat-cartoon-illustration">Image by freepik</a></small>
         },
         {
+            tag: "brokenimg1",
             src: "cat_img.jpg",
             attribution: ""
         },
         {
+            tag: "catimg2",
             src: catImg2,
             attribution: <small><a href="https://www.freepik.com/free-vector/hand-drawn-cat-cartoon-illustration">Image by freepik</a></small>
         },
         {
+            tag: "catimg3",
             src: catImg3,
             attribution: <small><a href="https://www.freepik.com/free-vector/hand-drawn-cat-cartoon-illustration">Image by freepik</a></small>
         },
         {
+            tag: "brokenimg2",
             src: "cat_img.jpg",
             attribution: ""
         },
         {
+            tag: "catimg4",
             src: catImg4,
             attribution: <small><a href="https://www.freepik.com/free-vector/hand-drawn-cat-cartoon-illustration">Image by freepik</a></small>
         }
@@ -40,7 +46,7 @@ const ImageGallery = () => {
         <CContainer>
             <CRow>
                 <CCol xs={12}>
-                    <CCard className='mb-4'>
+                    <CCard id='card1' className='mb-4'>
                         <CCardBody>Some images are visible, some are broken, and some may disappear every refresh</CCardBody>
                     </CCard>
                 </CCol>
@@ -49,7 +55,7 @@ const ImageGallery = () => {
                 {images.slice(0, 3).map((image, index) => (
                     <CCol xs={12} sm={6} md={4} className="image-col" key={index}>
                         {getRandomVisibility() ? (
-                            <div>
+                            <div id={image.tag}>
                                 <CImage rounded src={image.src} width={150} height={150} className="image" />
                                 <div>{image.attribution}</div>
                             </div>
@@ -63,7 +69,7 @@ const ImageGallery = () => {
                 {images.slice(3).map((image, index) => (
                     <CCol xs={12} sm={6} md={4} className="image-col" key={index + 3}>
                         {getRandomVisibility() ? (
-                            <div>
+                            <div id={image.tag}>
                                 <CImage rounded src={image.src} width={150} height={150} className="image" />
                                 <div>{image.attribution}</div>
                             </div>
@@ -76,16 +82,16 @@ const ImageGallery = () => {
 
             <CRow>
                 <CCol xs={12}>
-                    <CCard className='mb-4 mt-5'>
+                    <CCard id='card2' className='mb-4 mt-5'>
                         <CCardBody>These images below will always be visible</CCardBody>
                     </CCard>
                 </CCol>
             </CRow>
             <CRow className='mb-5'>
-                <CCol xs={12} md={6} className="d-flex justify-content-center align-items-center">
+                <CCol id='alwaysVisibleImg1' xs={12} md={6} className="d-flex justify-content-center align-items-center">
                     <CImage rounded src={catImg1} width={200} height={200} />
                 </CCol>
-                <CCol xs={12} md={6} className="d-flex justify-content-center align-items-center">
+                <CCol id='alwaysVisibleImg2' xs={12} md={6} className="d-flex justify-content-center align-items-center">
                     <CImage rounded src={catImg2} width={200} height={200} />
                 </CCol>
             </CRow>
